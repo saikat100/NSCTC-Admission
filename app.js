d7e00dd65e95650b3e7cf43e261427a199544a92
user <user@users-MacBook-Pro-2.local>
2021-08-10 10:52:05 -0700

    img added

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 "use strict";
 
+window.addEventListener("load", function () {
+	document
+		.querySelector('input[type="file"]')
+		.addEventListener("change", function () {
+			if (this.files && this.files[0]) {
+				var img = document.querySelector("img");
+				img.onload = () => {
+					URL.revokeObjectURL(img.src); // no longer needed, free memory
+				};
+
+				img.src = URL.createObjectURL(this.files[0]); // set src to blob url
+			}
+		});
+});
+
 // Booking conform block
 document.querySelector(".book-now").addEventListener("click", function () {
 	const slNo = document.querySelector(".sl-no").value;
